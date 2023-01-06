7a9d338ae5c3d7d1abdb62ee1821319c612c7c8e
Kunal <91754021+Ksbot007@users.noreply.github.com>
2023-01-06 09:01:30 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -16,13 +16,15 @@ class script(object):
 Myself <a href=https://t.me/{}>{}</a>,\n\nTrust me ! I can't even imagine how super-fast i can drive your Database channel \n\nAre you ready for Long Drive Baby...🤪''')
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝙼𝚈 𝙷𝙴𝙻𝙿 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
-    ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/LazyDeveloper>LazyDeveloper</a>
-✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
-✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: KOYEB
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v2.0.1 [ LazyPrincess ]"""
+    ABOUT_TXT = """✮ ᴍʏ ɴᴀᴍᴇ : ᴘʟᴜsᴛᴇᴄʜᴢ
+✮ ᴄʀᴇᴀᴛᴏʀ: ᴛᴇᴀᴍ | sʜᴀᴅᴏᴡ
+✮ ʟɪʙʀᴀʀʏ: ᴘᴜʀᴏɢʀᴀᴍ
+✮ ʟᴀɴɢᴜᴀɢᴇ: ᴘʏᴛʜᴏɴ 𝟹
+✮ ᴅᴀᴛᴀʙᴀsᴇ : ᴍᴏɴɢᴏ ᴅʙ
+✮ ʙᴏᴛ sᴇʀᴠᴇʀ: ʜᴇʀᴜᴋᴏ
+✮ ʙᴜɪʟᴅ sᴛᴀᴛs: ᴠ1.0.43
+✮ sᴜᴘᴘᴏʀᴛ: <a href=https://t.me/Flims4UChat>sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ</a>
+✮ ᴜᴘᴅᴀᴛᴇs: <a href=https://t.me/HDFlims4U>ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ</a>"""
     SOURCE_TXT = """<b>LazyPrincess is an open source project</b>
 
 You can easily get its source code from github - <a href='https://github.com/LazyDeveloperr/LazyPrincessV2'>LazyDeveloperr</a>"""
