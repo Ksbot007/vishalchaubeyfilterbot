1e5ea0f4b3a287d37dff3f820132dcd9e936cf55
Kunal <91754021+Ksbot007@users.noreply.github.com>
2023-01-06 12:46:51 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -435,10 +435,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('ʙᴏᴛs | ᴜᴘᴅᴀᴛᴇs', url='http://T.ME/PLUSTECHZ'),
-            InlineKeyboardButton('ғʟɪᴍs | ᴜᴘᴅᴀᴛᴇs', url='http://T.ME/HDFLIMS4U')
-            ],[
-            InlineKeyboardButton('🔎 sᴇᴀʀᴄʜ', url='http://T.ME/PLUSTECHZ'),
-            InlineKeyboardButton('☑️ ʜᴇʟᴘ', callback_data='help')
+            InlineKeyboardButton('ғʟɪᴍs | ᴜᴘᴅᴀᴛᴇs', url='http://T.ME/HDFLIMS4U')][InlineKeyboardButton('🔎 sᴇᴀʀᴄʜ', url='http://T.ME/PLUSTECHZ'),InlineKeyboardButton('☑️ ʜᴇʟᴘ', callback_data='help')
             ],[
             InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about'),
             InlineKeyboardButton('ɢʀᴏᴜᴘ', url='https://t.me/PlusFlimz')
