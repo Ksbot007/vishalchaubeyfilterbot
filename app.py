812e0ebf7e2b5c7deeefbdfdcffa8cafb667bd58
Kunal <91754021+Ksbot007@users.noreply.github.com>
2023-01-07 08:51:03 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import os
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-   return 'LazyDeveloper'
-
-os.system("git clone https://github.com/Ksbot007/vishalchaubeyfilterbot okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
