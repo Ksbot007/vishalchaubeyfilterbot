a4c431f7f6c5cfcaab14813d371062389f45ed68
Kunal <91754021+Ksbot007@users.noreply.github.com>
2023-04-15 22:10:48 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -259,7 +259,7 @@ async def start(client, message):
         caption=f_caption,
         reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('ᴊᴏɪɴ ɴᴏᴡ', url='https://t.me/PlusFlimz') ] ] ),
         protect_content=True if pre == 'filep' else False,
-        await asyncio.sleep(10)
+        await asyncio.sleep(10),
         await auek.delete()
        )
                     
