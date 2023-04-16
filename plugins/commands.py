2891b84948693fc4441db7f07ce3f14fe316ef26
Kunal <91754021+Ksbot007@users.noreply.github.com>
2023-04-16 09:39:28 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -253,14 +253,14 @@ async def start(client, message):
             f_caption=f_caption
     if f_caption is None:
         f_caption = f"{files.file_name}"
- oahs = await client.send_cached_media(
+    await client.send_cached_media(
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
         reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('ᴊᴏɪɴ ɴᴏᴡ', url='https://t.me/PlusFlimz') ] ] ),
         protect_content=True if pre == 'filep' else False,
        )await asyncio.sleep(10) 
-        await oahs.delete()
+        await f_caption.delete()
 
 @Client.on_message(filters.command('channel') & filters.user(ADMINS))
 async def channel_info(bot, message):
